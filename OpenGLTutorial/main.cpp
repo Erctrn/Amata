d9988248dc26e4129b1f768b7f788f324b8af8b0
Eric Tran <erictrann@hotmail.com>
2018-07-02 14:14:38 -0700

    Made vertex/fragment shaders and tested with square

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -3,6 +3,87 @@
 #include <iostream>
 #include "GameObject.h"
 #include "Circle.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2]; // vertex and fragment
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos) //set mode to vertex
+				type = ShaderType::VERTEX;
+
+			else if (line.find("fragment") != std::string::npos) //set mode to fragment
+				type = ShaderType::FRAGMENT;
+		}
+		else // if any other line of code, add it to vertex or fragment source code through stringstream
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+	return { ss[0].str(), ss[1].str() };  // once we've built up and read the source code of the file, return the struct
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str(); 
+	glShaderSource(id, 1, &src, nullptr); 
+	glCompileShader(id);
+
+	// Error handling
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) 
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) // static because dont want to leak to other translation units or cpp files
+{
+	// code to compile the two shaders
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader); 
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader); 
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 int main(void)
 {
@@ -28,6 +109,43 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	// draw square
+
+	float positions[] =
+	{
+		-0.5f, -0.5f, // Bot-left index 0
+		0.5f, -0.5f, // Bot-right index 1
+		0.5f,  0.5f, // Top-right index 2
+		-0.5f,  0.5f, // Top-left index 3
+	};//xcoord, ycoord - 2 floats per vertex
+
+	unsigned int indices[] =
+	{
+		0, 1, 2, // bottom right triangle
+		2, 3, 0  // top left triangle
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer); 
+	glBindBuffer(GL_ARRAY_BUFFER, buffer); 
+
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW); 
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	
+	unsigned int ibo; 
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+	ShaderProgramSource source = ParseShader("Basic.shader");
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	glUseProgram(shader);
+
+	// draw circle 
+
+
 	// Vu Test 
 	GameObject player;
 	Circle cir;
@@ -47,6 +165,8 @@ int main(void)
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
